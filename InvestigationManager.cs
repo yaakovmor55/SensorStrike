f60b29f289a890261f9f08a5d46f6e99c48a43ba
Yaakov <y0583275590@gmail.com>
2025-06-15 16:34:02 +0300

    Add MVP: JuniorSoldier agent with basic sensor functionality

---
+++ InvestigationManager.cs
@@ -0,0 +1,38 @@
+﻿using SensorStrike.Agents;
+using SensorStrike.Sensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorStrike
+{
+    public class InvestigationManager
+    {
+        private IraninAgent agent;
+
+        public InvestigationManager()
+        {
+            agent = new JuniorSoldier();
+        }
+
+        public  void Start()
+        {
+            Console.WriteLine("Beginning of investigation of junior Iranian agent");
+            while (!agent.IsExposed())
+            {
+                Console.WriteLine("Chois Sensor (Thermal / Audio):");
+                string UserInput = Console.ReadLine();
+
+                ISensor sensor = new BasicSensor(UserInput);
+                agent.AttachSensor(sensor);
+
+                string result = agent.GetActiveResult();
+                Console.WriteLine($"Result: {result}");
+            }
+            Console.WriteLine("The agent was successfully exposed.");
+
+        }
+    }
+}
---
+++ Sensors/ISensor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorStrike.Sensors
+{
+    public interface ISensor
+    {
+        
+        string Name { get; }
+        bool IsActive { get; }
+        void Activate();
+    
+    
+    }
+}
